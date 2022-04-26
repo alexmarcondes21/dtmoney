39ea028385c2ecb1677a277dc089e7f8e53e55bf
alexanderJr <alexmarcjr21@gmail.com>
2022-04-26 14:08:20 -0300

    Creating a context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,14 @@
+import React, { useContext } from "react";
 import { Container } from "./styles";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
+import { TransactionsContext } from "../../TransactionsContext";
 
 export function Summary() {
+  const transactions = useContext(TransactionsContext);
+
+  console.log(transactions)
   return (
     <Container>
       <div>
@@ -25,7 +30,7 @@ export function Summary() {
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>  R$500,00</strong>
+        <strong> R$500,00</strong>
       </div>
     </Container>
   );
